2c283a78b415ec03fdd24fd52700064e7fd3e0e4
Xiao Hanyu <xiaohanyu1988@gmail.com>
2026-08-28 21:15:48 +0800

    chore(playground): adopt sample resume for Playground web test page

--- packages/playground/web/src/App.tsx
+++ packages/playground/web/src/App.tsx
@@ -1,9 +1,18 @@
 import { Playground } from '@yamlresume/playground'
+import { getSampleResume } from '@yamlresume/samples'
+import { useState } from 'react'
 
 export default function App() {
+  const [resume, setResume] = useState(
+    getSampleResume('software-engineer', 'en', {
+      withComments: true,
+      withLayouts: true,
+    })
+  )
+
   return (
     <div className="h-screen w-screen">
-      <Playground />
+      <Playground yaml={resume} onChange={setResume} />
     </div>
   )
 }
